09f197e99976c6bef4d4c29033d26e6006a79f5c
Eric Pigeon <epigeon@workarea.com>
2019-06-14 22:43:53 -0400

    wip

--- spec/integration/chromiebara/driver_spec.rb
+++
@@ -1,9 +0,0 @@
-# require 'spec_helper'
-#
-# RSpec.describe Chromiebara::Driver do
-#   let(:session) { Capybara::Session.new(:chromiebara, TestApp) }
-#
-#   it 'allows the page to be scrolled' do
-#     session.visit('/apparition/long_page')
-#   end
-# end
